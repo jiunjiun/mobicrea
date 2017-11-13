7a1025f14be15117266fb455841ade9d29760489
jiunjiun <quietmes@gmail.com>
2017-11-13 16:19:38 +0800

    Add - Add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+
+  # 關於元皿
+  get 'about-us',                     to: 'about#us',             as: 'about_us'
+  # 服務內容
+  get 'service',                      to: 'service#index',        as: 'service'
+  get 'service_collection',           to: 'service#collection',   as: 'collection'
+  get 'service_furniture',            to: 'service#furniture',       as: 'furniture'
+  # 成功案例
+  get 'successful_story',             to: 'case#index',           as: 'stories'
+  get 'case',                         to: 'case#story',           as: 'case'
+  # 代理品牌
+  get 'acting_brand',                 to: 'brand#index',          as: 'acting_brand'
+  get 'segis',                        to: 'brand#segis',          as: 'segis'
+  # 聯絡方式
+  get 'contact-information',          to: 'contact#information',  as: 'information'
+  get 'contact-recruit',              to: 'contact#recruit',      as: 'recruit'
+
+  root 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
