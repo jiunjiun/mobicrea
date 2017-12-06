888d6bdef4c0eca17881c2908204a5ceeb6faa3c
jiunjiun <quietmes@gmail.com>
2017-12-06 16:47:38 +0800

    Refactor - Add GA code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = Settings.domain.url
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -88,4 +88,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: Settings.domain.host, port: Settings.domain.port }
+  Rails.application.routes.default_url_options[:host] = Settings.domain.host
+
+  GA.tracker = Settings.google.analytics_code
 end
