c5181bb1ef79507f0dca7db2588e5a2fd6c3e737
jiunjiun <quietmes@gmail.com>
2017-12-11 16:07:31 +0800

    Refactor - Add font-end page locale

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+  def current_locale
+    locales = {
+      'zh-TW': {
+        class_name: 'tw',
+        name: '繁體中文',
+      },
+      'en': {
+        class_name: 'en',
+        name: 'English',
+      }
+    }
+
+    locales[I18n.locale.to_sym]
+  end
+
+  def is_locale_zh_tw?
+    I18n.locale == :'zh-TW'
+  end
+
+  def locale_field resource, field
+    if is_locale_zh_tw?
+      resource.send("#{field}")
+    else
+      resource.send("#{field}_eng")
+    end
+  end
 end
