033d79e92f4b3c418f3e5fce69bfbdf31a09ccb0
jiunjiun <quietmes@gmail.com>
2018-01-11 17:01:33 +0800

    Refactor - Refine page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+
+  def success_cases_image_class
+    cycle(
+        'width2x height2x', 'grid-sizer', '', '', '',
+        '', '', 'width2x height2x', '', ''
+      )
+  end
 end
